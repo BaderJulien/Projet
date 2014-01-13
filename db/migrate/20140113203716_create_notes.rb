2df78352c370f02ef62ab290113f5b114859f144
Julien <baderjulien@hotmail.fr>
2014-01-13 22:55:35 +0100

    sqlit3

--- db/migrate/20140113203716_create_notes.rb
+++ db/migrate/20140113203716_create_notes.rb
@@ -2,9 +2,9 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.date :Date
-      t.int :Categorie
-      t.int :Duree
-      t.int :Difficulte
+      t.integer :Categorie
+      t.integer :Duree
+      t.integer :Difficulte
       t.string :Titre
       t.text :Texte
       t.text :Commentaire
